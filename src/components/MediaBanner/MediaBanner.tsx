b2f1cd6dff35a14857a6f2a98ab4b6386a623130
paulasaguz <paulajohanasaguz@gmail.com>
2023-12-08 09:29:20 -0600

    Update MediaBanner.tsx

--- src/components/MediaBanner/MediaBanner.tsx
+++ src/components/MediaBanner/MediaBanner.tsx
@@ -6,7 +6,7 @@ import Card from '../ui/Card';
 const MediaBanner = () => {
   const { data, error, isLoading } = useData();
   const { img, heading, orientation, button, leadingText } = data?.mediaBanner || {};
-  if (isLoading) return <p>Loading...</p>
+  if (isLoading) return <div className="min-h-[500px]"><p>Loading...</p></div>
   if (error) return <p>Whoops, something went wrong. Mind refreshing the page, please?"</p>
 
   return <Card img={img} heading={heading} leadingText={leadingText} orientation={orientation} button={button} />
